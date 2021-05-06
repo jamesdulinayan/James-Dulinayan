c4b2ca5c09ec3716b1a84b64534085d0fd8dd346
jamesdulinayan <jvmesdulinayan@gmail.com>
2021-05-06 11:54:11 +0800

    /

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ Load Page
 	});
 
 
-
 /*-------------------------------------------
 Ajax link page transitions
 ---------------------------------------------*/
@@ -43,11 +42,12 @@ Ajax link page transitions
             $('#ajax-content').html($(data).filter('#ajax-content').html());
             $('#ajax-sidebar').html($(data).filter('#ajax-sidebar').html());
 
+
 						$('body').waitForImages({
 							finished: function() {
 								Website();
 								backLoading();
-								$('.opacity-nav').delay(50).fadeOut(600);
+								$('.fat-nav').delay(50).fadeOut(600);
               },										
               waitForAll: true
 						});								
